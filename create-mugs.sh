7cc048d53fcd79391a42121bb4b5dffebaff92d1
Jasper <well.caffeinated@gmail.com>
2023-04-03 16:12:02 -0600

    maybe a good mug template script?

--- create-mugs.sh
+++ create-mugs.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+PROFILE="USWebCoatedSWOP.icc"
 TEMPLATE="ME Mug 1 - DFTBA template.jpg"
 OUTDIR="mugs"
 
 mkdir -p $OUTDIR
 
 for STICK in *.png; do
-  convert "$TEMPLATE" \( "$STICK" -flop -resize 45% -colorspace CMYK -geometry +1728+310 \) -composite -geometry 100% "$OUTDIR/MUG-$STICK"
+  magick "$TEMPLATE" \( "$STICK" -flop -resize 45% +profile sRGB -colorspace CMYK -black-point-compensation -intent Perceptual -profile "$PROFILE"  -geometry +1728+310 \) -composite -geometry 100% "$OUTDIR/MUG-${STICK%.png}.png"
+  convert -colorspace CMYK -intent Perceptual "$OUTDIR/MUG-${STICK%.png}.png" "$OUTDIR/MUG-${STICK%.png}.jpg"
 done
