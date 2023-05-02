cd289b5595bc8122175c516d908cae3910d959f1
Jasper <well.caffeinated@gmail.com>
2023-05-02 16:56:35 -0600

    no more flip image

--- create-mugs.sh
+++ create-mugs.sh
@@ -9,6 +9,6 @@ OUTDIR="mugs"
 mkdir -p $OUTDIR
 
 for STICK in *.png; do
-  magick "$TEMPLATE" \( "$STICK" -flop -resize 45% +profile sRGB -colorspace CMYK -black-point-compensation -intent Perceptual -profile "$PROFILE"  -geometry +1728+310 \) -composite -geometry 100% "$OUTDIR/MUG-${STICK%.png}.png"
+  magick "$TEMPLATE" \( "$STICK" -resize 45% +profile sRGB -colorspace CMYK -black-point-compensation -intent Perceptual -profile "$PROFILE"  -geometry +1728+310 \) -composite -geometry 100% "$OUTDIR/MUG-${STICK%.png}.png"
   convert -colorspace CMYK -intent Perceptual "$OUTDIR/MUG-${STICK%.png}.png" "$OUTDIR/MUG-${STICK%.png}.jpg"
 done
