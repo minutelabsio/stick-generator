d64c4d1c2386728f0642c22781ccc7b9a8650142
Jasper <well.caffeinated@gmail.com>
2023-05-09 13:41:29 -0600

    add filename hover

--- src/components/stick-generator/ImageSelector.svelte
+++ src/components/stick-generator/ImageSelector.svelte
@@ -39,14 +39,20 @@ async function getComposite(images, cropped){
   return await getMaybeCropped(ctx.canvas, cropped)
 }
 
+// takes the asset which is possibly an array and returns the filename without the path
+const getName = (asset) => {
+  if (Array.isArray(asset)){ return asset[0].split('/').pop() }
+  return asset.split('/').pop()
+}
+
 let lastImages
 async function updateImages(imagePromise, cropped){
   const images = await imagePromise
   if (lastImages === images){ return }
-  lastImages = images
+  lastImages = images.map(asset => ({ name: getName(asset), value: asset }))
   if (!Array.isArray(images)){ return }
   imageList = await Promise.all(images.map(async assets =>
-    ({ src: await getComposite(assets, cropped), value: assets }))
+    ({ src: await getComposite(assets, cropped), value: assets, name: getName(assets) }))
   )
 }
 
@@ -57,9 +63,9 @@ $: updateImages(images, cropped)
   {#await images}
     <p>Loading...</p>
   {:then images}
-    {#each imageList as {src, value}}
-      <div on:click={select(value)} class="carousel-item" class:selected="{selected === value}">
-        <img src={src} width="{imageWidth}"/>
+    {#each imageList as {src, value, name}}
+      <div on:click={select(value)} class="carousel-item" class:selected="{selected === value}"  data-alt={name}>
+        <img src={src} width="{imageWidth}" alt={name}/>
       </div>
     {/each}
   {:catch error}
@@ -74,16 +80,33 @@ $: updateImages(images, cropped)
   /* border: 1px solid #666; */
 }
 .carousel-item {
+  position: relative;
   filter: brightness(0.8) saturate(0.8);
   transition: filter 0.15s ease;
   background: white;
+  overflow: visible;
 }
 .carousel-item img {
   height: intrinsic;
   align-self: center;
 }
+.carousel-item:hover {
+  z-index: 1;
+}
 .carousel-item:hover,
 .carousel-item.selected {
   filter: brightness(1.25) saturate(1);
+}
+/* create popup for alt tags on images */
+.carousel-item:hover::after {
+  content: attr(data-alt);
+  position: absolute;
+  bottom: 0em;
+  left: 0;
+  padding: 0.25em;
+  background: white;
+  z-index: 3;
+  color: red;
+  width: max-content;
 }
 </style>
